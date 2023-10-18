5e7671f01f45f86ea705b5278695d50fc2a0e9a6
Yury Rakala <yuryrakala@gmail.com>
2023-10-18 21:43:58 +0300

    Front uploaded

--- src/main/java/com/Yury/Food_Delivery/controllers/UserController.java
+++ src/main/java/com/Yury/Food_Delivery/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.Yury.Food_Delivery.controllers;
 import com.Yury.Food_Delivery.models.User;
 import com.Yury.Food_Delivery.services.Impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserController {
 
     private final UserServiceImpl userService;
 
+    @CrossOrigin("http://localhost:3000")
     @GetMapping("/getUserList")
     public List<User> userList(Principal principal){
         return userService.userList();
